f4bf0069087e3bcd8387bda734b6b47d7be6fe28
Ilnur <ilnur.saipiyev@mail.ru>
2025-11-02 22:19:53 +0500

    final

--- script.js
+++ script.js
@@ -1,78 +1,77 @@
 $(document).ready(function() {
-    console.log("jQuery is ready!");
+  console.log("jQuery loaded successfully!");
 
-    // Task 1
-    $("#changeStyle").click(function() {
-        $("#byID").text("Selected by ID → Updated!");
-        $(".byClass").html("<b>Updated by Class!</b>");
-        $("h3").css({"color": "purple", "font-size": "22px"});
-    });
+  // Task 1 – Selectors
+  $("#title").text("✔ jQuery is working!");
+  $("#task1 p").css({
+    color: "#007bff",
+    fontWeight: "bold"
+  }).text("This text was changed using jQuery!");
 
-    // Task 2
-    $("#hideBtn").click(() => $("#textToHide").hide());
-    $("#showBtn").click(() => $("#textToHide").show());
-    $("#toggleBtn").click(() => $("#textToHide").toggle());
+  // Task 2 – Visibility
+  $("#hide-btn").click(() => $("#text").hide());
+  $("#show-btn").click(() => $("#text").show());
+  $("#toggle-btn").click(() => $("#text").toggle());
 
-    // Task 3
-    $("#fadeOutBtn").click(() => $("#fadeImage").fadeOut());
-    $("#fadeInBtn").click(() => $("#fadeImage").fadeIn());
-    $("#fadeToggleBtn").click(() => $("#fadeImage").fadeToggle());
+  // Task 3 – Fade
+  $("#fadeout-btn").click(() => $("#fade-img").fadeOut());
+  $("#fadein-btn").click(() => $("#fade-img").fadeIn());
+  $("#fadetoggle-btn").click(() => $("#fade-img").fadeToggle());
 
-    // Task 4
-    $("#togglePanelBtn").click(() => $("#panel").slideToggle());
+  // Task 4 – Slide
+  $("#slideup-btn").click(() => $("#collapsible-panel").slideUp());
+  $("#slidedown-btn").click(() => $("#collapsible-panel").slideDown());
+  $("#slidetoggle-btn").click(() => $("#collapsible-panel").slideToggle());
 
-    // Task 5
-    let count = 3;
-    $("#addItem").click(() => {
-        $("#itemList").append(`<li>Item ${count++}</li>`);
-    });
-    $("#removeItem").click(() => {
-        $("#itemList li:last").remove();
-    });
+  // Task 5 – Add/Remove
+  $("#add-item").click(() => $("ul").append("<li>New Item</li>"));
+  $("#remove-item").click(() => $("ul li:last-child").remove());
 
-    // Task 6
-    $("#changeSrc").click(() => {
-        $("#attrImage").attr("src", "https://via.placeholder.com/150/ff9999");
-    });
-    $("#changeHref").click(() => {
-        $("#myLink").attr("href", "https://www.wikipedia.org").text("Go to Wikipedia");
-    });
+  // Task 6 – Attributes
+  $("#change-img").click(() => {
+    $("#attr-img").attr("src", "https://picsum.photos/200?random=" + Math.floor(Math.random() * 100));
+  });
+  $("#change-link").click(() => {
+    $("#attr-link").attr("href", "https://wikipedia.org").text("Go to Wikipedia");
+  });
 
-    // Task 7
-    $("#nameInput").keyup(function() {
-        $("#liveName").text($(this).val());
-    });
-    $("#emailInput").keyup(function() {
-        $("#liveEmail").text($(this).val());
-    });
+  // Task 7 – Form Interaction
+  $("#name, #email").on("input", function() {
+    const name = $("#name").val();
+    const email = $("#email").val();
+    $("#live-output").html(`<b>Name:</b> ${name}<br><b>Email:</b> ${email}`);
+  });
 
-    // Task 8
-    $("#animateBtn").click(function() {
-        $("#box").animate({left: "200px", height: "150px", width: "150px"}, 800);
-    });
+  // Task 8–10 – Animations
+  $("#animate1").click(() => {
+    $("#square").animate({ left: "200px", height: "150px", width: "150px" }, 1000)
+                 .animate({ left: "0px", height: "100px", width: "100px" }, 1000);
+  });
 
-    // Task 9
-    $("#sequentialBtn").click(function() {
-        $("#box").animate({left: "200px"}, 600)
-                 .animate({top: "100px"}, 600)
-                 .animate({height: "50px", width: "50px"}, 600)
-                 .animate({left: "0px", top: "0px", height: "100px", width: "100px"}, 600);
-    });
+  $("#animate2").click(() => {
+    $("#square").animate({ left: "200px" }, 600)
+                .animate({ top: "150px" }, 600)
+                .animate({ left: "0" }, 600)
+                .animate({ top: "0" }, 600);
+  });
 
-    // Task 10
-    $("#combinedBtn").click(function() {
-        $("#box").animate({
-            left: "250px",
-            opacity: 0.5,
-            height: "120px",
-            width: "120px"
-        }, 800);
-    });
+  $("#animate3").click(() => {
+    $("#square").animate({
+      left: "150px",
+      opacity: 0.3,
+      height: "200px",
+      width: "200px"
+    }, 1000).animate({
+      left: "0px",
+      opacity: 1,
+      height: "100px",
+      width: "100px"
+    }, 1000);
+  });
 
-    // Task 11: Image Gallery
-    $(".gallery img").click(function() {
-        $(".gallery img").not(this).fadeTo(400, 0.3);
-        $(this).fadeTo(400, 1).css("transform", "scale(1.3)");
-        setTimeout(() => $(this).css("transform", "scale(1)"), 800);
-    });
+  // Task 11 – Gallery
+  $(".gallery img").click(function() {
+    $(".gallery img").removeClass("active");
+    $(this).addClass("active");
+  });
 });
